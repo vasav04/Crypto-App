675610c50f0e575267cb91110eafa65e91e010c6
unknown <vasavvasav111@gmail.com>
2023-08-26 10:18:59 +0530

    Hi

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCB78kEQlIXvtjO9yZ5tKNuQNbd3Y8bBvE",
+  authDomain: "crypto-view-df33a.firebaseapp.com",
+  projectId: "crypto-view-df33a",
+  storageBucket: "crypto-view-df33a.appspot.com",
+  messagingSenderId: "738290415161",
+  appId: "1:738290415161:web:f4d34692e05cebc004bfe8",
+  measurementId: "G-6XKCDXWGMR"
+};
+
+export default firebaseConfig;
